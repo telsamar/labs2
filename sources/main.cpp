007db0e17f43c80b99d4a88140eb30fcd775ba62
telsamar <Stolya2000@mail.ru>
2019-12-09 18:23:05 +0300

    //

--- sources/main.cpp
+++
@@ -1,9 +0,0 @@
-//Copyright 2019 <LinkIvan333>
-#include <Runner.hpp>
-int main() {
-    int a;
-    std::cout << "Введите количество уровней кэша = ";
-    std::cin >> a;
-    Runner Te(a);
-    return 0;
-}
--- tests/test.cpp
+++
@@ -1,7 +0,0 @@
-//Copyright 2019 <LinkIvan333>
-
-#include <gtest/gtest.h>
-
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
-}
